47aabe48f386db55509adf5c75dc1c1322bde5e7
pasa ismihan <pasa_13142@hotmail.com>
2023-03-07 11:33:54 +0300

    guncellendi

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,25 @@
-import React from 'react';
-
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 export default function KaydedilenlerListesi(props) {
+  let activeStyle = {
+    color: "red",
+  };
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+      {props.list.map((movie) => (
+        <span className="saved-movie">
+          <NavLink
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            to={`filmler/${movie.id}`}
+          >
+            {movie.title}
+          </NavLink>
+        </span>
       ))}
-      <div className="home-button">Anasayfa</div>
+      <Link to="/">
+        <div className="home-button">Anasayfa</div>
+      </Link>
     </div>
   );
 }
